d5c39dddbf9d62723ce7fc037a9de3421d088aed
Sebastian <sebastian.m.kaleta@gmail.com>
2019-04-12 23:16:53 +0200

    Adding if conditions

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 class InfoField extends React.Component {
   state = {
     level: "",
-    hight: "",
+    height: "",
     arms: "",
-    sex: ""
+    sex: "",
+    bowLength: ""
   };
 
   handleChange = e => {
@@ -12,6 +13,28 @@ class InfoField extends React.Component {
     });
   };
 
+  bowCalculate = (e, height) => {
+    if (this.state.height >= "152" && this.state.height <= "152") {
+      this.setState({
+        [e.target.name]: e.target.value,
+        bowLength: `60-64"`
+      });
+    } else if (this.state.height >= "165" && this.state.height <= "168") {
+      this.setState({
+        [e.target.name]: e.target.value,
+        bowLength: `65-66"`
+      });
+    } else if (this.state.height >= "170" && this.state.height <= "173") {
+      this.setState({
+        [e.target.name]: e.target.value,
+        bowLength: `67-68"`
+      });
+    } else if (this.height >= "175" && this.height <= "178") {
+      this.setState({
+        bowLength: `69-70"`
+      });
+    }
+  };
 
   render() {
     return (
@@ -21,17 +44,40 @@ class InfoField extends React.Component {
         <div>
           <label>
             Podaj rozpiętość ramion
-        <input className="form__input" name="arms" type="number" placeholder="0" min="0" max="200" value={this.props.arms} onChange={this.handleChange} />
+            <input
+              className="form__input"
+              name="arms"
+              type="number"
+              placeholder="0cm"
+              min="0"
+              max="200"
+              value={this.props.arms}
+              onChange={this.handleChange}
+            />
           </label>
           <br />
           <label>
             Podaj wzrost
-        <input className="form__input" name="hight" type="number" placeholder="0" min="0" max="300" value={this.state.hight} onChange={this.handleChange} />
+            <input
+              className="form__input"
+              name="height"
+              type="number"
+              placeholder="0cm"
+              min="0"
+              max="300"
+              value={this.state.height}
+              onChange={this.handleChange}
+            />
           </label>
           <br />
           <label>
             Podaj stopień zaawansowania
-        <select className="form__input" name="level" value={this.state.level} onChange={this.handleChange}>
+            <select
+              className="form__input"
+              name="level"
+              value={this.state.level}
+              onChange={this.handleChange}
+            >
               <option value="choose">Choose level</option>
               <option value="beginner">Beginner</option>
               <option value="medium">Medium</option>
@@ -41,15 +87,21 @@ class InfoField extends React.Component {
           <br />
           <label>
             Podaj płeć
-
-            <select className="form__input" name="sex" value={this.state.sex} onChange={this.handleChange}>
-              <option value="choose" >Choose sex</option>
-              <option value="male" >male</option>
-              <option value="female" >female</option>
+            <select
+              className="form__input"
+              name="sex"
+              value={this.state.sex}
+              onChange={this.handleChange}
+            >
+              <option value="choose">Choose sex</option>
+              <option value="male">male</option>
+              <option value="female">female</option>
             </select>
-            {/* Tu ma być dopisany wybór M/K w select lub checkbox */}
           </label>
           <br />
+          <h2 name="bowLength" onChange={this.bowCalculate}>
+            Twoje dane:{this.state.bowLength}
+          </h2>
         </div>
       </React.Fragment>
     );
